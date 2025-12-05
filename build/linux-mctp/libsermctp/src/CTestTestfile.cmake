e203cda2e6e4d1221fac9ea7b21ca44c926a45fe
dsandy12 <dlsandy@asu.edu>
2025-12-05 16:36:11 -0700

    remove build

--- build/linux-mctp/libsermctp/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doug/git/mctp-serial-linux/linux-mctp/libsermctp/src
-# Build directory: /home/doug/git/mctp-serial-linux/build/linux-mctp/libsermctp/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
